d07f4d6373704e3dddc1bdaba2c7d0ece7bbbff7
namithap6793 <namithaparramal@yahoo.in>
2017-03-30 09:25:34 +0530

    to find term frequency

--- freq.pl
+++ freq.pl
@@ -1,13 +1,13 @@
 $file=$ARGV[0];
 $word=$ARGV[1];
-open(my $fh,'<:encoding(UTF-8)',$file) or die "could not open filename '$file' $!";
+open(my $fh,"<:encoding(UTF-8)",$file) || die "could not open filename $file: $!";
 $total=0;
-while(my $line=<$fh>)
+while(my $line= <$fh>)
 {
-my @match=$line=- m/($word)/g;
+my @match=$line=~m/($word)/g;
 my $count=scalar(@match);
 $total=$total+$count;
-#print "$count\t$line\n";
+print "$count\t$line\n";
 }
 print "word frequency:$total\n";
 
